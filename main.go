cd9a9a20ce33e62ab1c877785adf25fbc8540098
Brendan McGarry <42652567+mcgarebear@users.noreply.github.com>
2024-02-05 20:30:47 -0500

    getting color for each pixel within the gif

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type sketchConfig struct {
 
 func main() {
 
+	// parse configuration from environment
 	const envconfigKey = "SKETCH"
 	var config sketchConfig
 	if err := envconfig.Process(envconfigKey, &config); err != nil {
@@ -24,12 +25,12 @@ func main() {
 		log.Fatal("Failed to process environment variables: " + err.Error())
 	}
 
+	// verify path and attempt to open file
 	if config.Path == "" {
 		envconfig.Usagef(envconfigKey, &config, os.Stderr, envconfig.DefaultTableFormat)
 		log.Fatal("Failed to open image at path: " + config.Path +
 			"; Path not provided.")
 	}
-
 	img, err := os.Open(config.Path)
 	if err != nil {
 		envconfig.Usagef(envconfigKey, &config, os.Stderr, envconfig.DefaultTableFormat)
@@ -37,6 +38,7 @@ func main() {
 			"; " + err.Error())
 	}
 
+	// decode open file into gif
 	gif, err := gif.DecodeAll(img)
 	if err != nil {
 		envconfig.Usagef(envconfigKey, &config, os.Stderr, envconfig.DefaultTableFormat)
@@ -48,14 +50,27 @@ func main() {
 	log.Println("gif.Config", gif.Config)
 	log.Println("gif.BackgroundIndex", gif.BackgroundIndex)
 
+	// for each image in the gif
 	numImages := len(gif.Image)
 	log.Println("numImages", numImages)
-
 	for idx := 0; idx < numImages; idx++ {
 		log.Println("idx", idx)
 		log.Println("gif.Image", gif.Image[idx])
 		log.Println("gif.Delay", gif.Delay[idx])
 		log.Println("gif.Disposal", gif.Disposal[idx])
+
+		image := gif.Image[idx]
+		// iterate through each pixel in the image, starting from the top left
+		// and moving to the bottom right. For each pixel, index into the color
+		// pallette to determine the pixel's color value.
+		for row := 0; row < gif.Config.Height; row++ {
+			for col := 0; col < gif.Config.Width; col++ {
+				pixelIdx := (row-image.Rect.Min.Y)*image.Stride + (col - image.Rect.Min.X)
+				color := image.Palette[image.Pix[pixelIdx]]
+				log.Println("x", col, "y", row, "color", color)
+			}
+		}
+
 	}
 
 }
